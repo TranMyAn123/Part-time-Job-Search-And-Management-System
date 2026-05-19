c75cd8d76d78fb85aa1c018dc4f1aefa1babdd6c
TranKhanhAn68 <2351050004an@ou.edu.vn>
2026-05-19 11:51:54 +0700

    Home Page

--- partimejobapp/screens/Home/Home.js
+++ partimejobapp/screens/Home/Home.js
@@ -1,11 +1,35 @@
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, ScrollView, View, StyleSheet } from "react-native";
 
-const Home = () => {
+import Header from "../../components/home/Header";
+import SearchBar from "../../components/SearchBar";
+import IndustryChip from "../../components/home/IndustryChip";
+// import Banner from "./components/Banner";
+// import FeaturedJobs from "./components/FeaturedJobs";
+// import NearbyJobs from "./components/NearbyJobs";
+// import BottomNav from "./components/BottomNav";
+
+export default function App() {
+  const [activeChip, setActiveChip] = useState("Tất cả");
+  const [activeNav, setActiveNav] = useState("home");
+
   return (
     <View>
-      <Text>HOME</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header />
+
+        <SearchBar />
+
+        <IndustryChip active={activeChip} setActive={setActiveChip} />
+
+
+        {/* <FeaturedJobs />
+
+        <NearbyJobs /> */}
+      </ScrollView>
+
+      {/* <BottomNav active={activeNav} setActive={setActiveNav} /> */}
     </View>
-  )
+  );
 }
 
-export default Home;
